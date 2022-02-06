278924780deacee1f618822fb0c2aa5bcc4a087d
Herick Portugues <herick.portugues@gmail.com>
2022-02-06 17:52:07 -0300

    Extraindo as rotas de cada cidade e salvando dados em json

--- src/data/crawler.js
+++ src/data/crawler.js
@@ -1,36 +1,73 @@
+const fs = require("fs");
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-class Crawler {
-    constructor() {
-        this.base_url = 'https://deonibus.com';
-    }
+const base_url = 'https://deonibus.com';
 
-    async scrapeData() {
-        try {
-            const start_url = this.base_url + '/rodoviaria';
-            const { data } = await axios.get(start_url);
-            const $ = cheerio.load(data);
-            const cities = $(".station.searchable-item");
-            cities.each((_, city) => {
-                const city_url = $(city).attr("href");
-                this.parseCityPage(city_url);
-            })
-        } catch (err) {
-            console.log(err);
-        }
-    }
+const scrapeData = async function () {
+    const busWays = [];
 
-    async parseCityPage(city_url) {
-        try{
-            const { data } = await axios.get(this.base_url + city_url);
-            const $ = cheerio.load(data);
-            const routes = $(".routes.leaving a");
-            console.log(routes.length);
-        } catch (err) {
-            console.log(err);
-        }
+    try {
+        const { data } = await axios.get(`${base_url}/rodoviaria`);
+        const $ = cheerio.load(data);
+
+        const cities = $(".station.searchable-item");
+        Promise.all(cities.map(async (_, city) => {
+            const city_url = $(city).attr("href");
+            const cityRoutes = await parseCityPage(city_url);
+            busWays.push(cityRoutes);
+        })).then(() => {
+            saveJson(busWays);
+        });
+    } catch (err) {
+        console.log(err);
     }
 }
 
-new Crawler().scrapeData();
+const parseCityPage = async function (city_url) {
+    try {
+        const { data } = await axios.get(base_url + city_url);
+        const $ = cheerio.load(data);
+
+        const cityTrips = [];
+        const routes = $(".routes.leaving a");
+        routes.map((_, route) => {
+            const trip = {
+                "destinationCity": getDestinationCity($, route),
+                "tripUrl": $(route).attr("href"),
+            }
+            cityTrips.push(trip);
+        });
+
+        return {
+            "originCity": getOriginCity($),
+            "cityTrips": cityTrips,
+        };
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const getOriginCity = function ($) {
+    let originCity = $("#main-logo span").text();
+    originCity = originCity.match(/de (.+)/)[1]
+    return originCity;
+}
+
+const getDestinationCity = function ($, routeSelector) {
+    let destinationCity = $(routeSelector).attr("title");
+    destinationCity = destinationCity.match(/para (.+)/)[1]
+    return destinationCity;
+}
+
+const saveJson = function (busWays) {
+    fs.writeFile("./src/data/data.json", JSON.stringify(busWays, null, 2), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Successfully written data to file");
+    });
+}
+
+scrapeData();
